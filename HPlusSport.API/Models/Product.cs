7f3958d879f5e62c11310c0c383617e4874dd485
moab5 <moabadejumat@gmail.com>
2024-08-19 14:27:36 +0400

    - Model binding and adding items with POST

--- HPlusSport.API/Models/Product.cs
+++ HPlusSport.API/Models/Product.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace HPlusSport.API.Models;
@@ -11,7 +12,8 @@ public class Product
     public decimal Price { get; set; }
     public bool IsAvailable { get; set; }
 
+    [Required]
     public int CategoryId { get; set; }
     [JsonIgnore]
-    public virtual Category Category { get; set; }
+    public virtual Category? Category { get; set; }
 }
