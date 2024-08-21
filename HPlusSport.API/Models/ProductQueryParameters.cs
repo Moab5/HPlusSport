e9d6ddbebae20b150f5095989215479a0edd0444
moab5 <moabadejumat@gmail.com>
2024-08-21 16:37:00 +0400

    - Filter by min and max price

---
+++ HPlusSport.API/Models/ProductQueryParameters.cs
@@ -0,0 +1,7 @@
+namespace HPlusSport.API.Models;
+
+public class ProductQueryParameters : QueryParameters
+{
+    public decimal? MinPrice { get; set; }
+    public decimal? MaxPrice { get; set; }
+}
