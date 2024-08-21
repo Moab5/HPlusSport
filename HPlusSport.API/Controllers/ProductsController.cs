4af9ef78011f926b06ca50373f6c2fb4f1d04f91
moab5 <moabadejumat@gmail.com>
2024-08-21 16:16:37 +0400

    - Update GetAllProducts to use pagination

--- HPlusSport.API/Controllers/ProductsController.cs
+++ HPlusSport.API/Controllers/ProductsController.cs
@@ -22,9 +22,14 @@ namespace HPlusSport.API.Controllers
         }
         
         [HttpGet]
-        public async Task<ActionResult> GetAllProducts()
+        public async Task<ActionResult> GetAllProducts([FromQuery]QueryParameters queryParameters)
         {
-            return Ok(await _context.Products.ToArrayAsync());
+            IQueryable<Product> products = _context.Products;
+
+            products = products
+                .Skip(queryParameters.Size * (queryParameters.Page - 1))
+                .Take(queryParameters.Size);
+            return Ok(await products.ToArrayAsync());
         }
 
         [HttpGet("{id}")]
